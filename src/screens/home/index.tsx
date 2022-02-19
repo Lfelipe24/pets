03deb5be1cd0e7c04c165abff54d322b7dff3701
Luis Felipe Fernandez <l.felipe200082@gmail.com>
2022-02-19 03:08:14 -0600

    home screen

---
+++ src/screens/home/index.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { StyleSheet, View, Text } from "react-native";
+
+export const Home: React.FC = () => {
+    return (
+        <View>
+            <Text>This is the home page</Text>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+    },
+  });
+  
